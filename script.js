fff23633446956a5ebfaa88e4216c74fa64bea20
rooosie1c <chaymaeberouaya@gmail.com>
2024-06-02 01:15:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleSubMenu(menuId) {
+    const submenu = document.getElementById(menuId);
+    if (submenu.style.display === "none" || submenu.style.display === "") {
+        submenu.style.display = "block";
+    } else {
+        submenu.style.display = "none";
+    }
+}
