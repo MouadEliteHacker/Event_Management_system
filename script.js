dda6af283ec54a091fd175e96cd8503171fafef0
MouadEliteHacker <mouad.benmas@gmail.com>
2024-06-10 01:37:21 +0100

    unnecessary files

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleSubMenu(menuId) {
-    const submenu = document.getElementById(menuId);
-    if (submenu.style.display === "none" || submenu.style.display === "") {
-        submenu.style.display = "block";
-    } else {
-        submenu.style.display = "none";
-    }
-}
